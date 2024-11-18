8437fe1b589f7e42d13c4f982a22493a6da83fd7
inhanbyeol94 <inhanbyeol94@gmail.com>
2024-11-18 22:32:32 +0900

    refactor:

--- src/admin/dto/admin-mgr.update.dto.ts
+++ src/admin/dto/admin-mgr.update.dto.ts
@@ -1,4 +1,4 @@
 import { IntersectionType, OmitType, PartialType } from '@nestjs/swagger';
 import { AdminEntity } from '../admin.entity';
 
-export class AdminMgrUpdateDto extends IntersectionType(PartialType(OmitType(AdminEntity, ['deletedAt']))) {}
+export class AdminMgrUpdateDto extends IntersectionType(PartialType(OmitType(AdminEntity, ['id', 'deletedAt', 'lastLoginAt']))) {}
